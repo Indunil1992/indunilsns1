c7ef5d93fd28dc41edb43a557ea738d56a190d46
Indunil1992 <indunil@adroitlogic.com>
2019-09-05 14:29:26 +0530

    List Topic Subscriptions

--- indunilsns1/lambda.js
+++ indunilsns1/lambda.js
@@ -2,16 +2,16 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
-    sns.getTopicAttributes({
+    sns.listSubscriptionsByTopic({
         TopicArn: 'arn:aws:sns:us-east-1:318300609668:testsnsindunil'
     }).promise()
         .then(data => {
-            console.log("display DATA");
+            console.log("dataaaa");
             console.log(data);
             // your code goes here
         })
         .catch(err => {
-            console.log("display error");
+            console.log("errorr");
             console.log(err);
             // error handling goes here
         });
